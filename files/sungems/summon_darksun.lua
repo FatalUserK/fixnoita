1dfe10edbc45618fcae0b7f6419a08f657f110de
UserK <111083281+FatalUserK@users.noreply.github.com>
2026-04-27 00:38:28 +0100

    finished sungem fix

--- files/sungems/summon_darksun.lua
+++ files/sungems/summon_darksun.lua
@@ -2,6 +2,10 @@ function material_area_checker_success()
 	local entity_id = GetUpdatedEntityID()
 	if GlobalsGetValue("MISC_DARKSUN_EFFECT") ~= "1" then
 		local x,y = EntityGetTransform(entity_id)
+
+		local sungem_entity = EntityGetClosestWithTag(x,y, "darksunrock")
+		if sungem_entity ~= 0 then x,y = EntityGetTransform(sungem_entity) EntityKill(sungem_entity) end
+
 		EntityLoad("data/entities/items/pickup/sun/newsun_dark.xml", x, y)
 		EntityLoad("data/entities/particles/image_emitters/chest_effect.xml", x, y)
 
--- files/sungems/summon_sun.lua
+++ files/sungems/summon_sun.lua
@@ -2,6 +2,10 @@ function material_area_checker_success()
 	local entity_id = GetUpdatedEntityID()
 	if GlobalsGetValue("MISC_SUN_EFFECT") ~= "1" then
 		local x,y = EntityGetTransform(entity_id)
+
+		local sungem_entity = EntityGetClosestWithTag(x,y, "sunrock")
+		if sungem_entity ~= 0 then x,y = EntityGetTransform(sungem_entity) EntityKill(sungem_entity) end
+
 		EntityLoad("data/entities/items/pickup/sun/newsun.xml", x, y)
 		EntityLoad("data/entities/particles/image_emitters/chest_effect.xml", x, y)
 
