7911e779adaf9e8c7f070a3c435b4a01b1fc9c57
UserK <111083281+FatalUserK@users.noreply.github.com>
2026-05-29 03:57:15 +0100

    Delete check_wand_spinny.lua

--- files/check_wand_spinny.lua
+++
@@ -1,8 +0,0 @@
-local entity_id = GetUpdatedEntityID()
-
-if EntityHasTag(EntityGetRootEntity(entity_id), "player_unit") then
-    local item_comp = EntityGetFirstComponent(entity_id, "ItemComponent")
-    if item_comp then
-        ComponentSetValue2(item_comp, "play_spinning_animation", true)
-    end
-end
